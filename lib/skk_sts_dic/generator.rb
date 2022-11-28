8537a7b2debb97f62e0249d36242a90517966398
akicho8 <akicho8@gmail.com>
2022-11-28 11:52:15 +0900

    [feat] 差分がわからないため UTF-8 版も追加

--- lib/skk_sts_dic/generator.rb
+++ lib/skk_sts_dic/generator.rb
@@ -40,23 +40,33 @@ module SkkStsDic
     end
 
     def items_output
-      body = @yomi_items.collect { |yomi, item| "#{yomi} /#{item}/\n" }
-      str = (Pathname("#{__dir__}/header.txt").readlines + body.sort).join
-      output_file.write(str.toeuc)
-      puts "write: #{output_file}"
+      body = @yomi_items.collect { |yomi, item| "#{yomi} /#{item}/\n" }.sort
       puts "count: #{body.count}"
+
+      [
+        [".euc-jp", :toeuc],
+        [".utf-8",  :toutf8],
+      ].each do |coding_ext, method|
+        file = output_file(coding_ext)
+        file.write((header + body).join.send(method))
+        puts "write: #{file}"
+      end
     end
 
     def yomi_for(source)
       HumanReader.new(source).to_s
     end
 
-    def output_file
-      Pathname("#{__dir__}/../../SKK-JISYO.sts.dic").expand_path
+    def output_file(coding_ext)
+      Pathname("#{__dir__}/../../SKK-JISYO.sts#{coding_ext}.dic").expand_path
     end
 
     def localization_dir
       Pathname("#{__dir__}/translate-the-spire/localization").expand_path
+    end
+
+    def header
+      Pathname("#{__dir__}/header.txt").readlines
     end
   end
 end
